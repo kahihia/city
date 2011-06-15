948c1a7a4d260b831af0b99f88de20fea90f8834
Samuel Ferguson <sam@peakxp.com>
2011-06-15 14:58:36 -0600

    added code to urls for main page

--- alpha/settings.py
+++ alpha/settings.py
@@ -128,6 +128,7 @@ INSTALLED_APPS = (
     'south',
     'taggit',
     'alpha.event',
+    'alpha.home',
 )
 
 # A sample logging configuration. The only tangible logging
--- alpha/urls.py
+++ alpha/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'alpha.views.home', name='home'),
+    url(r'^$', 'alpha.home.views.home', name='home'),
     # url(r'^alpha/', include('alpha.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
