d499b446bd2421512126dcbdff814b36bdc01d8e
Tony Arkles <t.arkles+hg@gmail.com>
2011-07-07 15:30:32 -0600

    fixing widget bug (replacing curly braces with real brackets)

--- alpha/common_settings.py
+++ alpha/common_settings.py
@@ -14,16 +14,6 @@ MANAGERS = ADMINS
 BASE_PATH = os.path.abspath(
     os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join(BASE_PATH, '..', 'alpha.db3'),                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
