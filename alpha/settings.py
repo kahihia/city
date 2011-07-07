18238eda2bb5138c5f3be6ed1023aa6d677158cd
Tony Arkles <t.arkles+hg@gmail.com>
2011-07-07 15:32:26 -0600

    adding dev local settings

---
+++ alpha/settings.py
@@ -0,0 +1,14 @@
+# local dev settings
+
+from common_settings import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': os.path.join(BASE_PATH, '..', 'alpha.db3'),                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
