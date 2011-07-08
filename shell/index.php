df6ecc8f3f30301bcdeea8ef6debe612ca0a1d94
Levi Bucsis <levi@peakxp.com>
2011-07-07 19:11:08 -0600

    shell site coding

---
+++ shell/index.php
@@ -0,0 +1,95 @@
+<? include 'header.php'; ?>
+
+	<div id="main">
+	
+		<aside id="tags">
+			<nav>
+				<ul>
+					<li><a href="" class="current">All Categories</a></li>
+					<li><a href="">Tag name</a></li>
+					<li><a href="">Tag name</a></li>
+					<li><a href="">Tag name</a></li>
+					<li><a href="">Tag name</a></li>
+					<li><a href="">Tag name</a></li>
+					<li><a href="">Tag name</a></li>
+					<li><a href="">Tag name</a></li>
+					<li><a href="">Tag name</a></li>
+					<li><a href="">Tag name</a></li>
+					<li><a href="">Tag name</a></li>
+					<li><a href="" class="more">More</a></li>
+				</ul>
+			</nav>
+		</aside>		
+		
+		<section class="body">
+		<!-- Each section should begin with a new h1 (not h2), and optionally a header -->
+		<!-- You can have more than one header/footer pair on a page -->
+			<header>
+			<h1>Today's Events</h1>
+			</header>
+	
+			<ul>
+				<li>
+					<img src="">
+					<a href="">Union Square Free Summer Salsa Concert Series</a><br>
+					<ul><li>6:00PM</li><li>Union Square</li><li>$5</li><li>Music</li></ul>				
+				</li>
+				<li>
+					<img src="">
+					<a href="">Union Square Free Summer Salsa Concert Series</a><br>
+					<ul><li>6:00PM</li><li>Union Square</li><li>$5</li><li>Music</li></ul>				
+				</li>
+				<li>
+					<img src="">
+					<a href="">Union Square Free Summer Salsa Concert Series</a><br>
+					<ul><li>6:00PM</li><li>Union Square</li><li>$5</li><li>Music</li></ul>				
+				</li>
+				<li>
+					<img src="">
+					<a href="">Union Square Free Summer Salsa Concert Series</a><br>
+					<ul><li>6:00PM</li><li>Union Square</li><li>$5</li><li>Music</li></ul>				
+				</li>
+				<li>
+					<img src="">
+					<a href="">Union Square Free Summer Salsa Concert Series</a><br>
+					<ul><li>6:00PM</li><li>Union Square</li><li>$5</li><li>Music</li></ul>				
+				</li>
+				<li>
+					<img src="">
+					<a href="">Union Square Free Summer Salsa Concert Series</a><br>
+					<ul><li>6:00PM</li><li>Union Square</li><li>$5</li><li>Music</li></ul>				
+				</li>
+				<li>
+					<img src="">
+					<a href="">Union Square Free Summer Salsa Concert Series</a><br>
+					<ul><li>6:00PM</li><li>Union Square</li><li>$5</li><li>Music</li></ul>				
+				</li>
+				<li>
+					<img src="">
+					<a href="">Union Square Free Summer Salsa Concert Series</a><br>
+					<ul><li>6:00PM</li><li>Union Square</li><li>$5</li><li>Music</li></ul>				
+				</li>
+				<li>
+					<img src="">
+					<a href="">Union Square Free Summer Salsa Concert Series</a><br>
+					<ul><li>6:00PM</li><li>Union Square</li><li>$5</li><li>Music</li></ul>				
+				</li>
+			</ul>
+
+		</section><!-- .body -->
+
+		<aside id="ads">
+			<p>Advertisement</p>
+			<ul>
+				<li><a href=""></a></li>
+				<li><a href=""></a></li>
+				<li><a href=""></a></li>
+				<li><a href=""></a></li>
+			</ul>
+		</aside>
+
+	</div> <!-- #main -->
+	</div> <!-- .wrapper -->
+	
+	
+<? include 'footer.php'; ?>
---
+++ shell/login.php
@@ -0,0 +1,21 @@
+<? include 'header.php'; ?>
+
+
+		<section class="body">
+
+			<form>
+			
+			
+			<label for="login">Login</label>
+			<input id="login" type="text">
+		
+			<label for="password">Password</label>			
+			<input id="password" type="password" class="password">
+			
+			<input type="submit">
+			
+			</form>
+
+		</section><!-- .body -->
+
+<? include 'footer.php'; ?>
