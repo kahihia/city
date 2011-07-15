9ef05e21ec85b9754941079852e33e720be88f73
Tony Arkles <t.arkles+hg@gmail.com>
2011-07-15 13:09:36 -0600

    building up calendar

---
+++ alpha/home/static/js/libs/newcal.js
@@ -0,0 +1,110 @@
+(function($) {
+
+   function createHead() {
+     var table = $('<table class="newcal-table"/>');
+     var content = (
+       '<thead>' +
+	 '<tr>' +
+	 '<td class="newcal-month-left">A</td>' +
+	 '<td class="newcal-month" colspan="5">July 2011</td>' +
+	 '<td class="newcal-month-right">B</td>' +
+	 '</tr>' +
+	 '</thead>');
+     table.html( content );
+     return table;
+   }
+
+   function createDays() {
+     var days = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'];
+     var html = ['<tr>'];
+     for (var i in days) {
+       if (days.hasOwnProperty(i)) {
+	 html.push('<td class="newcal-day-head">' + days[i] + '</td>');
+       }
+     }
+     html.push('</tr>');
+     return $( html.join("") );
+   }
+
+   function startOfThisMonth() {
+     var date = new Date();
+     var start = { 'day' : date.getDay(),
+		   'month' : date.getMonth(),
+		   'year' : date.getYear() };
+     return start;
+   }
+   window.sotm = startOfThisMonth;
+
+   function createCalendarArrayRows(start) {
+     var rows = [], current = [], i;
+     rows.push(current);
+     var now = new Date(start.year, start.month, 1);
+     
+     // insert blank entries to start off the month
+     for(i=0; i < start.day; i++) {
+       current.push('');
+     }
+
+     // fill the rest
+     while (now.getMonth() === start.month) {
+       if (current.length == 7) {
+	 current = [];
+	 rows.push(current);
+       }
+       current.push(now.getDate());
+       now = new Date(now.getYear(), now.getMonth(), now.getDate() + 1);
+     }
+     while (current.length < 7) {
+       current.push('');
+     }
+     return rows;
+   }
+   window.ccar = createCalendarArrayRows;
+
+   function createCalendarRows() {
+     
+   }
+   
+
+   function createTable() {
+     return (createHead()
+	     .append(createDays()));
+   };
+
+   /***
+    * Options for newcal:
+    *   onClick: function(date) - function to call when a date is selected
+    */
+   $.fn.newcal = function(options) {
+     return this.each(
+       function() {
+	 var div;
+	 var element = $(this);
+	 element.bind(
+	   'click',
+	   function(e) {
+	     if (div) return true;
+	     var offset = element.position();
+	     var padding = element.css('padding-left');
+	     div = $('<div />')
+	       .addClass('newcal-date-popup')
+	       .css(
+		 { position: 'absolute',
+		   left: offset.left,
+		   top: offset.top + element.height()
+		 })
+	       .bind('click', function(e) { e.stopPropagation(); e.preventDefault(); return false; });
+	     element.after(div);
+	     div.append( createTable() );
+	     e.preventDefault();
+	     return false;
+	   });
+	 $(document).bind(
+	   'click',
+	   function(e) {
+	     div.hide();
+	   });
+       });
+   };
+
+ })(jQuery);
