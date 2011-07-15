4a4b4f4b9bf9ed55f659c8e56db9ca09d54ac54d
Levi Bucsis <levi@peakxp.com>
2011-07-15 14:37:30 -0600

    added event detail page

---
+++ shell/event.php
@@ -0,0 +1,28 @@
+<? include 'header.php'; ?>
+
+
+	<div id="main">
+		<aside id="photo">
+			<img src="" width=250 height=350>
+		</aside>
+			
+		<section class="body noborder">
+
+			<h1>Event Name</h1>
+
+			<ul><li class="date"><a href="">Wednesday, August 12</a><br>5:00PM &mdash; 7:00PM</li><li class="loc"><a href="">TCU Place</a><br>100 20th St</li><li class="price">$5</li></ul>
+									
+			
+			<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.</p>
+
+			<p>Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. Aenean dignissim pellentesque felis.</p>
+
+			<p>Morbi in sem quis dui placerat ornare. Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu. Cras consequat.</p>
+
+			<ul class="tags"><li><a href="">Sports</a>, <a href="">Free</a></li></ul>
+
+		</section><!-- .body -->
+	</div> <!-- #main -->
+	</div> <!-- .wrapper -->
+
+<? include 'footer.php'; ?>
