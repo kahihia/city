f21b1f2524ffdfeb3c5b45e7a13351dd5f54f38f
Tony Arkles <t.arkles+hg@gmail.com>
2011-07-18 13:11:06 -0600

    adding bits for production

---
+++ alpha/settings_prod.py
@@ -0,0 +1,16 @@
+# prod server settings
+
+from common_settings import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'cityfusion',                      # Or path to database file if using sqlite3.
+        'USER': 'cityfusion',                      # Not used with sqlite3.
+        'PASSWORD': 'Tt5loh6RWrae',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
