c4bdd5ec7f0904e9707244dfb33616d5b8e4920f
Samuel Ferguson <sam@peakxp.com>
2011-07-20 16:49:57 -0600

    changed the form to add custom labels and classes

--- alpha/event/forms.py
+++ alpha/event/forms.py
@@ -3,41 +3,8 @@ from event.models import Event
 from alpha.event.fields import JqSplitDateTimeField
 from alpha.event.widgets import JqSplitDateTimeWidget
 from django import forms
+from django.utils.translation import ugettext_lazy as _                         
 
-# class HTML5DateTimeInput(forms.DateTimeInput):
-#     input_type = 'datetime'
-
-# class HTML5EmailInput(forms.TextInput):
-#     input_type = 'email'
-
-# class EventFormLoggedIn(forms.ModelForm):
-#     """
-#     Form for creating a new event
-#     Used if user is logged in already
-#     """
-#     class Meta:
-#         model = Event
-#         exclude = ('owner', 'authentication_key', 'slug', 'email')
-
-#     def __init__(self, *args, **kwargs):
-#         super(EventFormLoggedIn, self).__init__(*args,**kwargs)
-#         #self.fields['start_time'].widget = HTML5DateTimeInput(attrs={'class':'date_time'})
-
-#     description = CharField( widget=TextInput(attrs={'class':'wide', 'rows':'5'}))
-
-# class EventForm(forms.ModelForm):
-#     """
-#     Form for creating a new event
-#     Validates the event is not already created
-#     """
-#     def __init__(self, *args, **kwargs):
-#         super(EventForm, self).__init__(*args,**kwargs)
-#         self.fields['start_time'].widget = HTML5DateTimeInput(attrs={'class':'date_time'})
-#         self.fields['email'].widget = HTML5EmailInput()
-
-#     class Meta:
-#         model = Event
-#         exclude = ('owner', 'authentication_key', 'slug')
 
 def generate_form(*args):
     class HTML5DateTimeInput(forms.DateTimeInput):
@@ -51,9 +18,19 @@ def generate_form(*args):
     class _EventForm(forms.ModelForm):
         def __init__(self, *args, **kwargs):
             super(_EventForm, self).__init__(*args,**kwargs)
-            self.fields['start_time'].widget = HTML5DateTimeInput(attrs={'class':'date_time'})
-            self.fields['description'].widget = TextInput(attrs={'class':'wide', 'rows':'5'})
-            self.fields['email'].widget = HTML5EmailInput()
+            if 'email' in self.fields:
+                self.fields['email'].widget = HTML5EmailInput()
+            self.fields['name'].widget.attrs['class'] = 'text wide'
+            self.fields['name'].label = _(u'Event Name')
+            self.fields['location'].widget.attrs['class'] = 'text wide'
+            self.fields['location'].label = _(u'Location')
+            self.fields['start_time'].widget = HTML5DateTimeInput(attrs={'class':'text wide date'})
+            self.fields['end_time'].widget = HTML5DateTimeInput(attrs={'class':'text wide date'})
+            self.fields['start_time'].label = _(u'When')
+            self.fields['description'].widget = forms.widgets.Textarea( attrs={ 'class':'wide', 
+                                                                                'rows':5 } )
+            self.fields['tags'].widget.attrs['class'] = 'text wide'
+            self.fields['picture'].label = _(u'Image')
         class Meta:
             model = Event
             exclude = tuple(args)
