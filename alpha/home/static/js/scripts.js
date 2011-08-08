6a422d573a7831489ecba3526b07dbcec18e1888
Samuel Ferguson <sam@peakxp.com>
2011-08-08 11:51:52 -0600

    shows tags if one selected and hidden

--- alpha/home/static/js/scripts.js
+++ alpha/home/static/js/scripts.js
@@ -20,19 +20,16 @@ $('.addevent').click(function(remove_time) {
     remove_time.stopPropagation();
 });
 
+// Tags list in event_browse: More/less toggle buttons
 if ($('#tags ul').children().length > 10) {
     //this hides all the tags after the first ten.
     $('#tags ul li').slice(10).hide();
-
-
     $('#tags ul').append('<li><a href="#" class="more"><span class="tickbox"><span class="downtick"></span></span>More</a></li>');
-    $('#tags ul').find('li:last').click(function(){
-	$(this).siblings(':gt(10)').toggle();
-	$(this).toggle();
-    });
+    $('#tags ul').find('li:last').bind('click', function(){ $(this).siblings(':gt(10)').toggle(); $(this).toggle(); });
     $('#tags ul').append('<li><a href="#" class="more"><span class="tickbox"><span class="uptick"></span></span>Fewer</a></li>');
-    $('#tags ul').find('li:last').hide().click(function(){
-	$(this).siblings(':gt(10)').toggle();
-	$(this).toggle();
-    });
+    $('#tags ul').find('li:last').hide().bind('click', function(){ $(this).siblings(':gt(10)').toggle(); $(this).toggle(); });
+}
+
+if ( $('.active').filter(':hidden').length > 0) {
+    $('.more').filter(':visible').trigger('click');
 }
