264a6e5370063f97addd9bce737d7438ee8796bc
Samuel Ferguson <sam@peakxp.com>
2011-08-10 04:59:40 -0600

    converted javascript to static

--- alpha/home/static/js/scripts.js
+++ alpha/home/static/js/scripts.js
@@ -22,12 +22,8 @@ $('.addevent').click(function(remove_time) {
 
 // Tags list in event_browse: More/less toggle buttons
 if ($('#tags ul').children().length > 10) {
-    //this hides all the tags after the first ten.
-    $('#tags ul li').slice(10).hide();
-    $('#tags ul').append('<li><a href="#" class="more"><span class="tickbox"><span class="downtick"></span></span>More</a></li>');
-    $('#tags ul').find('li:last').bind('click', function(){ $(this).siblings(':gt(10)').toggle(); $(this).toggle(); });
-    $('#tags ul').append('<li><a href="#" class="more"><span class="tickbox"><span class="uptick"></span></span>Fewer</a></li>');
-    $('#tags ul').find('li:last').hide().bind('click', function(){ $(this).siblings(':gt(10)').toggle(); $(this).toggle(); });
+    $('#tags_more').bind('click', function(){ $(this).siblings(':gt(10)').toggle(); $(this).toggle(); });
+    $('#tags_less').bind('click', function(){ $(this).siblings(':gt(10)').toggle(); $(this).toggle(); });
 }
 
 if ( $('.active').filter(':hidden').length > 0) {
