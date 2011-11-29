d9df89ff213029d87d250d1e1ad2122f2eaa61e5
Tony Arkles <t.arkles+hg@gmail.com>
2011-11-29 12:32:32 -0600

    sending the url for the correct image now

--- alpha/event/views.py
+++ alpha/event/views.py
@@ -236,7 +236,7 @@ def view(request, slug=None, old_tags=None):
     opengraph = { 'og:title' : event.name,
                   'og:type' : 'event',
                   'og:locale' : 'en_US',
-                  'og:image' : 'http://' + settings.EVENT_EMAIL_SITE + event_picture_url(event),
+                  'og:image' : 'http://' + settings.EVENT_EMAIL_SITE + event_picture_url(event, size=200),
                   'og:url' : 'http://' + settings.EVENT_EMAIL_SITE + reverse('event_view', args=(event.slug,)),
                   'og:site_name' : 'Cityfusion',
                   'fb:app_id' : '330171680330072',
