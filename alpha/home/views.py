f2fdc459a60177bf9488e3b112d6bcb59c4de0ae
Tony Arkles <t.arkles+hg@gmail.com>
2011-11-29 11:33:58 -0600

    experimenting with facebook connect

--- alpha/home/views.py
+++ alpha/home/views.py
@@ -1,6 +1,6 @@
 # Create your views here.
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 
 def custom_404(request):
@@ -11,3 +11,7 @@ def home(request):
 
 def redirect(request):
     return HttpResponseRedirect( reverse('home'))
+
+# for facebook connect
+def channelfile(request):
+    return HttpResponse('''<script src="//connect.facebook.net/en_US/all.js"></script>''')
