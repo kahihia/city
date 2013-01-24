509444e33494956e3512b12f63a28fdb9596860e
jaromudr <jaromudr@gmail.com>
2013-01-25 01:32:35 +0200

    Fix selectable. Change simplesjon to json

--- alpha/selectable/base.py
+++ alpha/selectable/base.py
@@ -9,7 +9,7 @@ from django.core.urlresolvers import reverse
 from django.core.serializers.json import DjangoJSONEncoder
 from django.http import HttpResponse
 from django.db.models import Q
-from django.utils import simplejson as json
+import json
 from django.utils.encoding import smart_unicode
 from django.utils.html import conditional_escape
 from django.utils.translation import ugettext as _
