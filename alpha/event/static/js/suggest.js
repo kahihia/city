f7be41ffd391ff8b987f1ecbf2b03c5a724abd76
jaromudr <jaromudr@gmail.com>
2013-01-24 23:47:31 +0200

    Location widget, change logic

--- alpha/event/static/js/suggest.js
+++ alpha/event/static/js/suggest.js
@@ -1,6 +1,19 @@
 (function($) {
     $(document).ready(function() {
 
+        $("[data-switch-to='suggest-tr']").on("click", function(){
+            $(".location-tr").hide();
+            $(".suggest-tr").show();
+        });
+
+        $("[data-switch-to='location-tr']").on("click", function(){            
+            $(".suggest-tr").hide();
+            $("#id_venue_name").val("");
+            $("#id_street").val("");
+            $("#id_city_0").val("");
+            $(".location-tr").show();
+        });
+
         function panMapToCenter(lat, lng) {
             var point = new google.maps.LatLng(lat, lng);
             google.maps.event.trigger(map_location, 'resize');
