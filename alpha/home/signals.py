786155f790b6220927be893236820bed99640704
Alexandr Chigrinets <alexandr.chigrinets@silkcode.com>
2013-08-14 12:28:17 +0600

    fixing bug with extended facebook token

--- alpha/home/signals.py
+++ alpha/home/signals.py
@@ -3,6 +3,8 @@ from django.contrib.auth.signals import user_logged_in
 
 def after_login(sender, user, request, **kwargs):
     if request.GET.get('facebook_login', False):
-        user.get_profile().extend_access_token()
+    	profile = user.get_profile()
+    	if not profile.new_token_required:
+        	user.get_profile().extend_access_token()
 
 user_logged_in.connect(after_login)
