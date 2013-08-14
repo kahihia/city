c9f6bb62aa8fa4494d0009328b15e5afd76fccb1
Alexandr Chigrinets <alexandr.chigrinets@silkcode.com>
2013-08-15 00:41:46 +0600

    changing facebook page id

--- alpha/event/settings.py
+++ alpha/event/settings.py
@@ -2,6 +2,6 @@ from django.conf import settings
 
 DEFAULT_FROM_EMAIL = getattr(settings, 'DEFAULT_FROM_EMAIL', u'noreply@cityfusion.ca')
 EVENT_PICTURE_DIR = getattr(settings, 'EVENT_PICTURE_DIR', 'pictures')
-FACEBOOK_PAGE_ID = getattr(settings, 'FACEBOOK_PAGE_ID', '523227901077594')
+FACEBOOK_PAGE_ID = getattr(settings, 'FACEBOOK_PAGE_ID', '638979249459268')
 EVENTFUL_ID = getattr(settings, 'EVENTFUL_ID', '294833066685')
 CONCERTIN_ID = getattr(settings, 'CONCERTIN_ID', '229535758760')
