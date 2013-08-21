e34040d5ef37bbc60cc5decb64d6ec63662782b6
Alexandr Chigrinets <alexandr.chigrinets@silkcode.com>
2013-08-22 01:53:58 +0600

    using django-configurations

--- alpha/manage.py
+++ alpha/manage.py
@@ -2,9 +2,9 @@
 import os
 import sys
 
-if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+if __name__ == '__main__':
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings"')
+    os.environ.setdefault('DJANGO_CONFIGURATION', 'Local')
 
-    from django.core.management import execute_from_command_line
-
+    from configurations.management import execute_from_command_line
     execute_from_command_line(sys.argv)
