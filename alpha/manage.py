bc96426ace524021b2a53cab3c6ded04364ee9ef
Alexandr Chigrinets <alexandr.chigrinets@silkcode.com>
2013-08-22 03:44:22 +0600

    manage.py rollback

--- alpha/manage.py
+++ alpha/manage.py
@@ -2,9 +2,9 @@
 import os
 import sys
 
-if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
-    os.environ.setdefault('DJANGO_CONFIGURATION', 'Local')
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
-    from configurations.management import execute_from_command_line
+    from django.core.management import execute_from_command_line
+
     execute_from_command_line(sys.argv)
