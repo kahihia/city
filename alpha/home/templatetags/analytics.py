e71eb220cf64bf0e432c63e1d8eded1fc4659446
Vladimir Myskov <jaromudr@gmail.com>
2013-08-22 13:31:07 +0300

    Try analytics on dev server

--- alpha/home/templatetags/analytics.py
+++ alpha/home/templatetags/analytics.py
@@ -19,9 +19,9 @@ class ShowGoogleAnalyticsJS(template.Node):
         return """
         <script type="text/javascript" data-turbolinks-eval=false>
             var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-            document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+            document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'  data-turbolinks-eval=false %3E%3C/script%3E"));
         </script>
-        <script type="text/javascript">
+        <script type="text/javascript" data-turbolinks-eval=false>
             try {
             var pageTracker = _gat._getTracker('""" + str(code) + """');
             pageTracker._trackPageview();
