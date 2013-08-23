110d5e3f849292013610ec842ca4368930119a45
Alexandr Chigrinets <alexandr.chigrinets@silkcode.com>
2013-08-24 01:02:25 +0600

    correcting like button styles

--- alpha/home/templatetags/common_tags.py
+++ alpha/home/templatetags/common_tags.py
@@ -48,5 +48,5 @@ def like_button(context, url):
     return '<iframe src="https://www.facebook.com/plugins/like.php?locale=en_US&amp;href=%s&amp;width=93' \
            '&amp;height=21&amp;colorscheme=light&amp;layout=button_count&amp;action=like&amp;show_faces=false' \
            '&amp;send=false&amp;appId=%s" scrolling="no" frameborder="0" style="border:none; ' \
-           'overflow:hidden; width:80px; height:21px; vertical-align: middle;" allowTransparency="true">' \
+           'overflow:hidden; width:90px; height:21px; vertical-align: middle;" allowTransparency="true">' \
            '</iframe>' % (urllib.quote(url), settings.FACEBOOK_APP_ID)
