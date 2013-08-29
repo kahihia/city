0f215ff814aba1585af7503b5f39f7b02dce2790
Alexandr Chigrinets <alexandr.chigrinets@silkcode.com>
2013-08-29 23:21:16 +0600

    changing urlize filter

--- alpha/home/templatetags/common_tags.py
+++ alpha/home/templatetags/common_tags.py
@@ -1,47 +1,18 @@
 import urllib
+import re
 
-from django.template.defaultfilters import stringfilter
-from django.utils.html import urlize
-from django.utils.safestring import mark_safe
 from django import template
 from django.conf import settings
-
-from BeautifulSoup import BeautifulSoup
 
 register = template.Library()
 
 
-def html_urlize(value, autoescape=None):
-    """Converts URLs in html text into clickable links."""
-    ignored_tags = ['a', 'code', 'pre']
-    soup = BeautifulSoup(value, convertEntities='html')
-    tags = soup.findAll(True)
-    text_all = soup.contents
-    for text in text_all:
-        if text not in tags:
-            parsed_text = urlize(text, nofollow=True, autoescape=autoescape)
-            text.replaceWith(parsed_text)
-    for tag in tags:
-        if not tag.name in ignored_tags:
-            soup_text = BeautifulSoup(str(tag))
-            if len(soup_text.findAll()) > 1:
-                for child_tag in tag.contents:
-                    child_tag.replaceWith(html_urlize(str(child_tag)))
-            elif len(soup_text.findAll()) > 0:
-                text_list = soup_text.findAll(text=True)
-                for text in text_list:
-                    parsed_text = urlize(text, nofollow=True, autoescape=autoescape)
-                    text.replaceWith(parsed_text)
-                try:
-                    tag.replaceWith(str(soup_text))
-                except Exception:
-                    pass
-    return mark_safe(str(soup))
+@register.filter
+def html_urlize(value):
+    pattern = re.compile(r'(^|[>\n ])(([\w]+?://[\w\#$%&~.\-;:=,?@\[\]+]*)(/[\w\#$%&~/.\-;:=,?@\[\]+]*)?)',
+                         re.IGNORECASE | re.DOTALL)
+    return pattern.sub(r'\1<a href="\2">\3</a>', value)
 
-html_urlize.is_safe = True
-html_urlize.needs_autoescape = True
-html_urlize = stringfilter(html_urlize)
-register.filter(html_urlize)
 
 @register.simple_tag(takes_context=True)
 def like_button(context, url):
