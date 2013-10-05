b581f26e8468baa4e57651b53569726fe2b798fb
vova <jaromudr@gmail.com>
2013-10-05 21:53:40 +0300

    Bonus campaigns coding finished

--- alpha/cityfusion_admin/forms.py
+++ alpha/cityfusion_admin/forms.py
@@ -1,6 +1,20 @@
 from django import forms
 from djmoney.forms.fields import MoneyField
+from accounts.models import BonusCampaign
 
 
 class FreeTryForm(forms.Form):
     bonus_budget = MoneyField()
+
+class BonusCampaignForm(forms.ModelForm):
+    bonus_budget = MoneyField()
+    start_time = forms.DateField(widget=forms.DateInput(format='%m/%d/%Y'))
+    end_time = forms.DateField(widget=forms.DateInput(format='%m/%d/%Y'))
+
+    class Meta:
+        model = BonusCampaign
+        fields = (
+            'start_time',
+            'end_time',
+            'bonus_budget'
+        )
