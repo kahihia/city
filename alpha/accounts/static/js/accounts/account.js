2dd6df5736ec0c693c9cc977cb4df79aa17265d9
Alexandr Chigrinets <alexandr.chigrinets@silkcode.com>
2013-10-25 23:19:46 +0600

    adding hintbox in the FB posting

--- alpha/accounts/static/js/accounts/account.js
+++ alpha/accounts/static/js/accounts/account.js
@@ -38,6 +38,7 @@
 
             self.reset();
             self.makeCheckBoxesForFBPosting();
+            self.initBalloons();
         }
 
         self.onExecuteButtonClick = function() {
@@ -346,6 +347,24 @@
 
                     $(this).find(self.eventCheckerSelector + "," + self.facebookLinkSelector).remove();
                     $(this).prepend(check);
+            });
+        };
+
+        self.initBalloons = function(){
+            $.balloon.defaults.classname = "hintbox";
+            $.balloon.defaults.css = {};
+            var ballons = $(".balloon");
+            $(ballons).each(function(){
+                var content = $(this).siblings(".balloon-content").clone().show();
+                $(this).balloon({
+                    contents:content,
+                    position:"top",
+                    tipSize: 0,
+                    offsetX:0,//$.browser.msie?0:25,
+                    offsetY:10,//$.browser.msie?25:0,
+                    showDuration: 500, hideDuration: 0,
+                    showAnimation: function(d) { this.fadeIn(d); }
+                });
             });
         };
 
