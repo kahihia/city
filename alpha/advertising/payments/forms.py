43025ff4f45f95e5ffe664225c897abc5a5cff18
Vladimir Myskov <jaromudr@gmail.com>
2013-10-28 16:49:32 +0200

    Show order history on fund advertising page

---
+++ alpha/advertising/payments/forms.py
@@ -0,0 +1,98 @@
+from django import forms
+from advertising.forms import AdvertisingSetupForm
+from djmoney.forms.fields import MoneyField
+from moneyed import Money, CAD
+
+
+class BaseFundForm(forms.Form):
+    order_budget = MoneyField(required=False)
+    bonus_budget = MoneyField(required=False)
+
+    def __init__(self, account, *args, **kwargs):
+        super(BaseFundForm, self).__init__(*args, **kwargs)
+        self.account = account
+
+
+class BaseSetupForm(AdvertisingSetupForm):
+    order_budget = MoneyField(required=False)
+    bonus_budget = MoneyField(required=False)
+
+
+class PaidFundForm(BaseFundForm):
+    order_budget = MoneyField(min_value=Money(10, CAD))
+    bonus_budget = MoneyField(required=False)
+
+    def __init__(self, *args, **kwargs):
+        super(PaidFundForm, self).__init__(*args, **kwargs)
+
+        self.fields['order_budget'].error_messages['min_value'] = 'Ensure budget is greater than or equal to %(limit_value)s'
+
+
+class PaidSetupForm(AdvertisingSetupForm):
+    order_budget = MoneyField(min_value=Money(10, CAD))
+    bonus_budget = MoneyField(required=False)
+
+    def __init__(self, *args, **kwargs):
+        super(PaidFundForm, self).__init__(*args, **kwargs)
+
+        self.fields['order_budget'].error_messages['min_value'] = 'Ensure budget is greater than or equal to %(limit_value)s'
+
+
+class BonusPaymentsMixin(object):
+    def clean(self):
+        cleaned_data = super(BonusPaymentsMixin, self).clean()
+
+        bonus_budget = cleaned_data["bonus_budget"]
+
+        if bonus_budget > self.account.bonus_budget:
+            raise forms.ValidationError('Ensure budget is lower than or equal to %s' % self.account.bonus_budget)
+
+        return cleaned_data
+
+
+    def save(self, commit=True):
+        campaign = super(BonusPaymentsMixin, self).save(commit=False)
+
+        cleaned_data = self.clean()
+
+        campaign.budget = cleaned_data["bonus_budget"]
+
+        if commit:
+            campaign.save()
+
+        return campaign
+
+
+class BonusFundForm(forms.Form, BonusPaymentsMixin):
+    order_budget = MoneyField(required=False)
+    bonus_budget = MoneyField(min_value=Money(10, CAD))
+
+    def __init__(self, account, *args, **kwargs):        
+        super(BonusFundForm, self).__init__(*args, **kwargs)
+
+        self.account = account
+
+        self.fields['bonus_budget'].error_messages['min_value'] = 'Ensure budget is greater than or equal to %(limit_value)s'
+
+
+class BonusSetupForm(AdvertisingSetupForm, BonusPaymentsMixin):
+    order_budget = MoneyField(required=False)
+    bonus_budget = MoneyField(min_value=Money(10, CAD))
+
+
+SETUP_FORMS = {
+    "paypal": PaidSetupForm,
+    "bonus": BonusSetupForm
+}
+
+FUND_FORMS = {
+    "paypal": PaidFundForm,
+    "bonus": BonusFundForm
+}
+
+
+def setup_form(name):
+    return SETUP_FORMS[name]
+
+def fund_form(name):
+    return FUND_FORMS[name]
---
+++ alpha/advertising/payments/processors.py
@@ -0,0 +1,89 @@
+from django.db.models import F
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+from advertising.models import AdvertisingOrder, AdvertisingCampaign, BonusAdvertisingTransaction
+from accounts.models import Account, AccountTaxCost
+from decimal import Decimal
+
+class BasePaymentProcessor(object):
+    def __init__(self, account, campaign, request):
+        self.account = account
+        self.campaign = campaign        
+        self.request = request
+
+    def process_setup(self):
+        raise NotImplementedError
+
+    def process_fund(self):
+        raise NotImplementedError        
+
+    def redirect_to_next_step(self):
+        raise NotImplementedError
+
+
+class PaypalPaymentProcessor(BasePaymentProcessor):
+    def process_setup(self):
+        order_budget = Decimal(self.request.POST["order_budget"])
+        total_price = order_budget
+
+        for tax in self.account.taxes():
+            total_price = total_price + (order_budget * tax.tax)
+
+        order = AdvertisingOrder(
+            budget=order_budget,
+            total_price=total_price,
+            campaign=self.campaign,
+            account=self.account
+        )
+
+        order.save()
+
+        for tax in self.account.taxes():
+            account_tax_cost = AccountTaxCost(account_tax=tax, cost=order_budget*tax.tax, tax_name=tax.name)
+            account_tax_cost.save()
+            order.taxes.add(account_tax_cost)
+
+        self.order = order
+
+    def process_fund(self):
+        self.process_setup()
+
+    def redirect_to_next_step(self):
+        return HttpResponseRedirect(reverse('advertising_payment', args=(str(self.order.id), )))     
+
+
+class BonusPaymentProcessor(BasePaymentProcessor):
+    def process_setup(self):
+        budget = Decimal(self.request.POST["bonus_budget"])
+        BonusAdvertisingTransaction.objects.create(
+            campaign=self.campaign,
+            budget=budget
+        )
+
+        Account.objects.filter(user_id=self.request.user.id).update(bonus_budget=F("bonus_budget")-budget)
+        AdvertisingCampaign.objects.filter(id=self.campaign.id).update(budget=F("budget")+budget)
+
+    def process_fund(self):
+        self.process_setup()
+
+    def redirect_to_next_step(self):
+        return HttpResponseRedirect(reverse('advertising_deposit_funds_for_campaign', args=(self.campaign.id, )))
+
+
+PAYMENT_PROCESSORS = {
+    "paypal": PaypalPaymentProcessor,
+    "bonus": BonusPaymentProcessor
+}
+
+
+def process_setup_campaign(payment_module, account, campaign, request):
+    processor = PAYMENT_PROCESSORS[payment_module](account, campaign, request)
+
+    processor.process_setup()
+    return processor.redirect_to_next_step()
+
+def process_fund_campaign(payment_module, account, campaign, request):
+    processor = PAYMENT_PROCESSORS[payment_module](account, campaign, request)
+
+    processor.process_fund()
+    return processor.redirect_to_next_step()    
