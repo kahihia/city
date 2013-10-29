bbb6f72fbe9aadd62ae87fc0ab8403d344daec97
Alexandr Chigrinets <alexandr.chigrinets@silkcode.com>
2013-10-29 22:40:55 +0600

    fixing bug with the notice mail sending

--- alpha/notices/services.py
+++ alpha/notices/services.py
@@ -1,5 +1,6 @@
 import json
 
+from django.contrib.sites.models import Site
 from django.template.loader import render_to_string
 from django.core.mail.message import EmailMessage
 
@@ -11,6 +12,8 @@ def create_notice(notice_type, user, mail_data={}, notice_data={}):
     log = json.dumps(notice_data)
     Notice.objects.create(type=notice_type, user=user, log=log)
 
+    current_site = Site.objects.get_current().domain
+    mail_data['site'] = current_site
     message = render_to_string('mail/%s.txt' % notice_type, mail_data)
 
     msg = EmailMessage(mail_data['subject'],
