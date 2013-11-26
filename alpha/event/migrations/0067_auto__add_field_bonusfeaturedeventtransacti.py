7762b688ca79aab3997ae6e654870bcb9ae6fc8f
Vladimir Myskov <jaromudr@gmail.com>
2013-11-26 18:49:42 +0200

    Fix event migration

--- alpha/event/migrations/0067_auto__add_field_bonusfeaturedeventtransacti.py
+++ alpha/event/migrations/0067_auto__add_field_bonusfeaturedeventtransacti.py
@@ -8,15 +8,6 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        # Adding model 'SingleEventOccurrence'
-        db.create_table(u'event_singleeventoccurrence', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('start_time', self.gf('django.db.models.fields.DateTimeField')()),
-            ('end_time', self.gf('django.db.models.fields.DateTimeField')()),
-            ('description', self.gf('django.db.models.fields.TextField')(null=True, blank=True)),
-        ))
-        db.send_create_signal(u'event', ['SingleEventOccurrence'])
-
         # Adding field 'BonusFeaturedEventTransaction.order'
         db.add_column(u'event_bonusfeaturedeventtransaction', 'order',
                       self.gf('django.db.models.fields.related.OneToOneField')(to=orm['event.FeaturedEventOrder'], unique=True, null=True),
@@ -24,9 +15,6 @@ class Migration(SchemaMigration):
 
 
     def backwards(self, orm):
-        # Deleting model 'SingleEventOccurrence'
-        db.delete_table(u'event_singleeventoccurrence')
-
         # Deleting field 'BonusFeaturedEventTransaction.order'
         db.delete_column(u'event_bonusfeaturedeventtransaction', 'order_id')
 
