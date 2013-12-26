7c18c691b6f83ba316f4542110f3f9e664534f8a
vova <jaromudr@gmail.com>
2013-12-26 23:22:02 +0200

    Optimize css and js for most often used pages

---
+++ alpha/advertising/assets/edit-advertising-combine.js
@@ -0,0 +1,7 @@
+//= require js/jquery.balloon
+//= require js/searchpad/jquery.qap_dropdown
+//= require js/create_event/venue_account_owner
+//= require js/advertising/total-price-calculation
+//= require js/date-range
+//= require fancybox/jquery.fancybox
+//= require js/advertising/setup-page
