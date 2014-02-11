b09f103e1759bb0ae2a843e964b5b38a7c730059
Alexandr Chigrinets <alexandr.chigrinets@silkcode.com>
2014-02-11 13:56:00 +0600

    adding 'last login' for users table in admin panel

--- alpha/accounts/admin.py
+++ alpha/accounts/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django import forms
+from userena.utils import get_user_model
+from userena.admin import UserenaAdmin
+from userena import settings as userena_settings
 from accounts.models import AccountReminding, InTheLoopSchedule, VenueAccount, VenueType, AccountTax, AccountTaxCost
 
 
@@ -12,9 +15,22 @@ class VenueAccountAdmin(admin.ModelAdmin):
         return db_field.formfield(**kwargs)
 
 
+class CityFusionUserAdmin(UserenaAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name',
+                    'is_staff', 'is_active', 'date_joined', 'last_login')
+
+
 admin.site.register(AccountReminding)
 admin.site.register(AccountTax)
 admin.site.register(AccountTaxCost)
 admin.site.register(InTheLoopSchedule)
 admin.site.register(VenueType)
 admin.site.register(VenueAccount, VenueAccountAdmin)
+
+if userena_settings.USERENA_REGISTER_USER:
+    try:
+        admin.site.unregister(get_user_model())
+    except admin.sites.NotRegistered:
+        pass
+
+    admin.site.register(get_user_model(), CityFusionUserAdmin)
