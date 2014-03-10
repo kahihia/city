e1255be210a301cfca1bf6e99a2bb198e3bc10e6
Alexandr Chigrinets <alexandr.chigrinets@silkcode.com>
2014-03-11 00:36:49 +0600

    changing the 'Reply-To' header in feedback

--- alpha/feedback/views.py
+++ alpha/feedback/views.py
@@ -4,7 +4,9 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.core.mail import mail_managers
+from django.core.mail.message import EmailMultiAlternatives
 from django.core.urlresolvers import reverse
+from django.conf import settings
 
 from home.models import Page
 from .forms import ContactForm
@@ -17,11 +19,17 @@ def feedback(request):
         if form.is_valid():
             hostname = request.get_host()
             time = datetime.datetime.now()
-            message_data = form.cleaned_data.copy()
-            message_data['time'] = time.strftime('%d-%m-%Y, %-1I:%M %p')
-            message = '''From: %(name)s <%(email)s>\n\nType: %(type)s\n\nTime: %(time)s
-                        \nText: %(comments)s''' % message_data
-            mail_managers('%s / Feedback' % hostname, message, fail_silently=True)
+
+            if settings.MANAGERS:
+                subject = '%s / Feedback' % hostname
+                message_data = form.cleaned_data.copy()
+                message_data['time'] = time.strftime('%d-%m-%Y, %-1I:%M %p')
+                message = '''From: %(name)s <%(email)s>\n\nType: %(type)s\n\nTime: %(time)s
+                            \nText: %(comments)s''' % message_data
+                mail = EmailMultiAlternatives('%s%s' % (settings.EMAIL_SUBJECT_PREFIX, subject),
+                                              message, message_data['email'], [a[1] for a in settings.MANAGERS],
+                                              headers = {'Reply-To': message_data['email']})
+                mail.send(fail_silently=True)
 
             feedback = Feedback(time=time, **form.cleaned_data)
             feedback.save()
