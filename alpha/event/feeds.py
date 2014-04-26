c901ec0d9187d999f0652cf8adaee2345eb161e0
Alexandr Chigrinets <alexandr@wps.su>
2014-04-26 14:59:29 +0600

    changing rss generation

--- alpha/event/feeds.py
+++ alpha/event/feeds.py
@@ -15,7 +15,7 @@ from .model_decorators import SingleEventModelDecorator
 
 
 class RssFusionFeed(Rss201rev2Feed):
-    extra_elements = ['image', 'cost', 'venue', 'location']
+    extra_elements = ['image', 'date', 'time', 'cost', 'venue', 'location']
 
     def add_item_elements(self, handler, item):
         super(RssFusionFeed, self).add_item_elements(handler, item)
@@ -43,11 +43,11 @@ class EventFeed(Feed):
         return super(EventFeed, self).get_object(request, args, kwargs)
 
     def get_feed(self, obj, request):
-        cache_key = 'rss_feed_%s' % self._tag_name
-        feed = cache.get(cache_key)
-        if not feed:
-            feed = super(EventFeed, self).get_feed(obj, request)
-            cache.set(cache_key, feed, 900) # caching for 15 minutes
+        #cache_key = 'rss_feed_%s' % self._tag_name
+        #feed = cache.get(cache_key)
+        #if not feed:
+        feed = super(EventFeed, self).get_feed(obj, request)
+        #cache.set(cache_key, feed, 900) # caching for 15 minutes
         return feed
 
     def items(self):
@@ -55,9 +55,10 @@ class EventFeed(Feed):
             event_ids_with_tags = Event.events.filter(
                 tagged_items__tag__name__in=[self._tag_name]
             ).values_list('id', flat=True)
-            single_events = SingleEvent.homepage_events.filter(event_id__in=list(event_ids_with_tags))
+            single_events = SingleEvent.homepage_events.filter(event_id__in=list(event_ids_with_tags))\
+                .select_related('event__venue')
         else:
-            single_events = SingleEvent.homepage_events.all()[:100]
+            single_events = SingleEvent.homepage_events.select_related('event__venue').all()
         return [SingleEventModelDecorator(single_event) for single_event in single_events]
 
     def item_title(self, item):
@@ -76,6 +77,8 @@ class EventFeed(Feed):
             u'image': '%s%s' % (self._domain, reverse('get_event_image', kwargs={'slug': item.slug,
                                                                                  'width': 650,
                                                                                  'height': 550})),
+            u'date': item.date(),
+            u'time': item.time(),
             u'cost': item.price(),
             u'venue': item.venue.name,
             u'location': item.venue.address
